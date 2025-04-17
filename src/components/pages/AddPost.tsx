76c482d14b1ff7d04f3c05678ac1a08798223529
Paulius19Ka <kallabow2@gmail.com>
2025-04-17 15:13:26 +0300

    added add form bones

--- src/components/pages/AddPost.tsx
+++ src/components/pages/AddPost.tsx
@@ -1,7 +1,50 @@
+import { useFormik } from 'formik';
+import { v4 as genID } from 'uuid';
+import * as Yup from 'yup';
+import { useNavigate } from 'react-router';
+import { useContext, useState } from 'react';
+
+import UsersContext from '../contexts/UsersContext';
+import { PostsContextTypes, UsersContextTypes } from '../../types';
+import PostsContext from '../contexts/PostsContext';
+
 const AddPost = () => {
+
+  const { loggedInUser } = useContext(UsersContext) as UsersContextTypes;
+  const { addPost } = useContext(PostsContext) as PostsContextTypes;
+  const [error, setError] = useState('');
+
+  const formik = useFormik({
+    initialValues: {
+      title: ''
+    },
+    onSubmit: (values) => {
+      console.log(values)
+    },
+  })
+
   return (
     <section>
       <h2>AddPost</h2>
+      <form onSubmit={formik.handleSubmit}>
+        <div>
+          <label htmlFor="title">Title:</label>
+          <input
+            type="text"
+            id='title' name='title'
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+            placeholder='enter title...'
+          />
+          {
+            formik.errors.title && formik.touched.title && <p style={{ color: "red" }}>{formik.errors.title}</p>
+          }
+        </div>
+        <input type="submit" />
+        {
+          error && <p style={{ color: "red" }}>{error}</p>
+        }
+      </form>
     </section>
   );
 }
