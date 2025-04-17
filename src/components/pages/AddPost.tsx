e87fe0ca98ea8487daaa028eae08dffcb6fc31fd
Paulius19Ka <kallabow2@gmail.com>
2025-04-17 15:20:15 +0300

    finished add form inputs

--- src/components/pages/AddPost.tsx
+++ src/components/pages/AddPost.tsx
@@ -16,7 +16,9 @@ const AddPost = () => {
 
   const formik = useFormik({
     initialValues: {
-      title: ''
+      title: '',
+      picture: '',
+      content: ''
     },
     onSubmit: (values) => {
       console.log(values)
@@ -34,10 +36,35 @@ const AddPost = () => {
             id='title' name='title'
             onBlur={formik.handleBlur}
             onChange={formik.handleChange}
-            placeholder='enter title...'
+            placeholder='enter the title...'
           />
           {
             formik.errors.title && formik.touched.title && <p style={{ color: "red" }}>{formik.errors.title}</p>
+          }
+        </div>
+        <div>
+          <label htmlFor="picture">Picture:</label>
+          <input
+            type="url"
+            id='picture' name='picture'
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+            placeholder='enter a picture url...'
+          />
+          {
+            formik.errors.picture && formik.touched.picture && <p style={{ color: "red" }}>{formik.errors.picture}</p>
+          }
+        </div>
+        <div>
+          <label htmlFor="content">Content:</label>
+          <textarea
+            id='content' name='content'
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+            placeholder='enter the content...'
+          />
+          {
+            formik.errors.content && formik.touched.content && <p style={{ color: "red" }}>{formik.errors.content}</p>
           }
         </div>
         <input type="submit" />
